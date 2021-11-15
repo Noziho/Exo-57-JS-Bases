6523c30620d0ba998acc503e8bc5b704215ca5ef
Noah2211 <noah.decroix3@gmail.com>
2021-11-15 14:16:32 +0100

    exo5

---
+++ exo 5/app.js
@@ -0,0 +1,6 @@
+let nombre1 = prompt("Saisissez un nombre à virgule (.):");
+let nombre2 = prompt("Saisissez un nombre à virgule (.):");
+let result = (parseInt(nombre1) * nombre2);
+
+
+alert(result)
